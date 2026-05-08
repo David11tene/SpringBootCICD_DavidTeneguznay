eb28a394d27bf2112858c187393df40f7d364ecb
DAVID TENEGUZÑAY <drteneguznay@espe.edu.ec>
2026-05-08 15:32:09 -0500

    shouldNotAllowDuplicatedEmail

---
+++ src/test/java/ec/edu/espe/springlab/service/StudentServiceTest.java
@@ -0,0 +1,42 @@
+package ec.edu.espe.springlab.service;
+
+import ec.edu.espe.springlab.domain.Student;
+import ec.edu.espe.springlab.dto.StudentCreateRequest;
+import ec.edu.espe.springlab.repository.StudentRepository;
+import ec.edu.espe.springlab.service.impl.StudentServiceImpl;
+import ec.edu.espe.springlab.web.advice.ConflictException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DataJpaTest
+@Import({StudentServiceImpl.class})
+public class StudentServiceTest {
+    @Autowired
+    private StudentServiceImpl service;
+
+    @Autowired
+    private StudentRepository repository;
+
+    @Test
+    void shouldNotAllowDuplicatedEmail() {
+        Student existing = new Student();
+        existing.setFullName("Existing");
+        existing.setEmail("duplicated@duplicated.com");
+        existing.setBirthDate(LocalDate.of(2004,10,12));
+        existing.setActive(true);
+        repository.save(existing);
+
+        StudentCreateRequest req =  new StudentCreateRequest();
+        req.setFullName("New user");
+        req.setEmail("duplicated@duplicated.com");
+        existing.setBirthDate(LocalDate.of(2004,10,12));
+
+        assertThatThrownBy(() -> service.create(req)).isInstanceOf(ConflictException.class);
+    }
+}
